3e4b584d828505cbc8bde7dfdc8288446f4225a2
Ankit1841 <62699027+Ankit1841@users.noreply.github.com>
2020-09-03 17:48:07 +0530

    Initialising question sub-folders (2 questions of dynam pro)

---
+++ Dynamic Prg/Max-subset non-adj sum/main.py
@@ -0,0 +1,25 @@
+# O (N) T | O(N) S | New array of len N created so O(N) S
+def maxSubsetNoAdjacentSum(array):
+    if len(array) == 0:
+        return 0
+    elif len(array) == 1:
+        return array[0]
+    maxSums = array[:]
+    maxSums[1] = max(array[0], array[1])
+    for i in range(2, len(array)):
+        maxSums[i] = max(maxSums[i-2]+array[i], maxSums[i-1])
+
+
+# O(N) T | O(1) S
+def maxSubsetNoAdjacentSum(array):
+    if len(array) == 0:
+        return 0
+    elif len(array) == 1:
+        return array[0]
+    second = array[0]
+    first = max(array[0], array[1])
+    for i in range(2, len(array)):
+        current = max(second+array[i], first)
+        second = first
+        first = current
+    return first
